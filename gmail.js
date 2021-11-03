ead395a3ab942408aff9d017ed78581eddf96b32
eduardostefanelloschumann <eduardo.stefanello@schumann.com.br>
2021-11-03 15:04:17 -0300

    Update gmail.js

--- gmail.js
+++ gmail.js
@@ -1,7 +1,7 @@
 setTimeout(function(){
-    document.getElementsByClassName('nH')[0].setAttribute('style', 'width: 1520px;')
-    document.getElementsByClassName('bq9 buW')[0].setAttribute('style', 'width: 400px;')
-    document.getElementsByClassName('brC-brG')[0].setAttribute('style', 'width: 400px;')
+    document.getElementsByClassName('nH')[0].setAttribute('style', 'width: 1500px;')
+    document.getElementsByClassName('bq9 buW')[0].setAttribute('style', 'width: 420px;')
+    document.getElementsByClassName('brC-brG')[0].setAttribute('style', 'width: 420px;')
 
     document.getElementsByClassName('nH oy8Mbf nn aeN')[0].setAttribute('style', 'min-width: 356px!important;')
 }, 6000);
